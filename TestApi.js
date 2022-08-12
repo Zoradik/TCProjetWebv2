5d39fa95b2bf6cfbd143460283b9f078a0afb416
Zoradik <56223677+Zoradik@users.noreply.github.com>
2022-08-12 11:34:27 +0200

    Test intégration de l'API + config de la bdd

--- TestApi.js
+++ TestApi.js
@@ -1,36 +1,23 @@
-﻿<!doctype html>
-<html lang="fr">
-<head>
-    <meta charset="utf-8">
-    <title>MindShop</title>
-    <link rel="stylesheet" href="./assets/vendors/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="./assets/vendors/fontawesome/css/all.min.css">
-    <link rel="stylesheet" href="./nav&footer.css" class="css">
-    <link rel="stylesheet" href="./ListesdeProduit.css" class="css">
-    <!-- <meta name="viewport"
-           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-     <meta http-equiv="X-UA-Compatible" content="ie=edge"> -->
-
-    <link rel="stylesheet" href="C:/Users/jerem/source/repos/TCProjetWebv2/ListesdeProduit.css" class="css">
-
-    <link Rel="Stylesheet" href="Https://Stackpath.Bootstrapcdn.Com/Bootstrap/4.3.1/Css/Bootstrap.Min.Css">
-    <link Rel="Stylesheet" href="Https://Cdnjs.Cloudflare.Com/Ajax/Libs/Font-Awesome/5.9.0/Css/All.Css">
-
-    <script src="./assets/vendors/jquery/jquery-3.6.0.min.js"></script>
-    <script src="TestApi.js"></script>
+﻿var callBackGetSuccess = function (data) {
+    console.log("donnees api" , data)
+}
 
 
-</head>
+function buttonClickGet() {
+  var url = "https://world.openfoodfacts.org/";
+  $("button").click(function () {
+    $.post("demo_test_post.asp",
+      {
+        name: "Donald Duck",
+        city: "Duckburg"
+      },
+      function (data, status) {
+        alert("Data: " + data + "\nStatus: " + status);
+      });
+  });
+}
 
-<?php include('nav.php'); ?>
-<body>
 
-<input type"button" value ="APPEL API" onclick="buttonClickFet()" />
-<p id="zone_API" ></p>
-
-    <div id="Script" data-href="https://world.openfoodfacts.org?json=true" </div>
- <!--
- <script>
 {
   "count": 118,
   "page": 1,
@@ -6758,13 +6745,5 @@
       "url": "http://world.productopener.localhost/product/3900003354612"
     }
   ],
-  "skip": 0 ;
+  "skip": 0
 }
-
-
-</script>
- -->
-
-    <?php include('footer.php'); ?>
-</body>
-</html>
