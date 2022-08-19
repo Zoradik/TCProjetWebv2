76f6b1d023a4da11c4765b4c7e66c129d719c7aa
Zoradik <56223677+Zoradik@users.noreply.github.com>
2022-08-19 14:47:43 +0200

    Utilisation de l'api pour trouver tout les produits

--- Api.php
+++ Api.php
@@ -1,10 +1,18 @@
 <?php
-$curl = curl_init('https://world.openfoodfacts.org?json=true');
-curl_setopt($curl, CURLOPT_CAINFO, __DIR__ . DIRECTORY_SEPARATOR . 'cert.cer');
-$data = curl_exec($curl) ;
-if ($data === false){
+$curl = curl_init('https://world.openfoodfacts.org/api/v2/product/{3017620422003}');
+curl_setopt_array($curl, [
+    CURLOPT_CAINFO         => __DIR__ . DIRECTORY_SEPARATOR . 'cert.cer',
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_TIMEOUT => 10
+]);
+$data = curl_exec($curl);
+if ($data === false) {
     var_dump(curl_error($curl));
 } else {
+    if (curl_getinfo($curl, CURLINFO_HTTP_CODE)) {
+        $data = json_decode($data, true);
 
+        echo 'Produit disponibles : ' . $data["product"]['brands_imported'];
+    }
 }
 curl_close($curl);
