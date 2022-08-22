bcff6a9ce651bcba3c7ea7e45b8f231049cdef24
Zoradik <56223677+Zoradik@users.noreply.github.com>
2022-08-22 22:31:29 +0200

    Ca marche

--- Accueil.php
+++ Accueil.php
@@ -57,39 +57,39 @@ $forecast = $GET->getForecast('3017620422003');
                         <div class="onestlabis">
                             <div class="twitter">
                                 <div class="separation1">
-                                <div class="col-12">
-                                    <ul>
-                                        <li> <a href="#!" style="color: #a2c4c9; " class="link"> <?= $forecast['Nom0'] ?></a></li>
-                                        <li> <a href="#!" style="color: #a2c4c9; " class="link"> <?= $forecast['Nom1'] ?></a></li>
-                                        <li> <a href="#!" style="color: #a2c4c9; " class="link"> <?= $forecast['Nom2'] ?></a></li>
-                                        <li> <a href="#!" style="color: #a2c4c9; " class="link"> <?= $forecast['Nom3'] ?></a></li>
-                                        <li> <a href="#!" style="color: #a2c4c9; " class="link"> <?= $forecast['Nom4'] ?></a></li>
-                                        <li> <a href="#!" style="color: #a2c4c9; " class="link"> <?= $forecast['Nom5'] ?></a></li>
-                                        <li> <a href="#!" style="color: #a2c4c9; " class="link"> <?= $forecast['Nom6'] ?></a></li>
-                                        <li> <a href="#!" style="color: #a2c4c9; " class="link"> <?= $forecast['Nom7'] ?></a></li>
-                                        <li> <a href="#!" style="color: #a2c4c9; " class="link"> <?= $forecast['Nom8'] ?></a></li>
-                                        <li> <a href="#!" style="color: #a2c4c9; " class="link"> <?= $forecast['Nom9'] ?></a></li>
-                                        <li> <a href="#!" style="color: #a2c4c9; " class="link"> <?= $forecast['Nom10'] ?></a></li>
-                                        <li> <a href="#!" style="color: #a2c4c9; " class="link"> <?= $forecast['Nom11'] ?></a></li>
-                                    </ul>
+                                    <div class="col-12">
+                                        <ul>
+                                            <li> <a href="#!" style="color: #a2c4c9; " class="link"> <?= $forecast['Nom0'] ?></a></li>
+                                            <li> <a href="#!" style="color: #a2c4c9; " class="link"> <?= $forecast['Nom1'] ?></a></li>
+                                            <li> <a href="#!" style="color: #a2c4c9; " class="link"> <?= $forecast['Nom2'] ?></a></li>
+                                            <li> <a href="#!" style="color: #a2c4c9; " class="link"> <?= $forecast['Nom3'] ?></a></li>
+                                            <li> <a href="#!" style="color: #a2c4c9; " class="link"> <?= $forecast['Nom4'] ?></a></li>
+                                            <li> <a href="#!" style="color: #a2c4c9; " class="link"> <?= $forecast['Nom5'] ?></a></li>
+                                            <li> <a href="#!" style="color: #a2c4c9; " class="link"> <?= $forecast['Nom6'] ?></a></li>
+                                            <li> <a href="#!" style="color: #a2c4c9; " class="link"> <?= $forecast['Nom7'] ?></a></li>
+                                            <li> <a href="#!" style="color: #a2c4c9; " class="link"> <?= $forecast['Nom8'] ?></a></li>
+                                            <li> <a href="#!" style="color: #a2c4c9; " class="link"> <?= $forecast['Nom9'] ?></a></li>
+                                            <li> <a href="#!" style="color: #a2c4c9; " class="link"> <?= $forecast['Nom10'] ?></a></li>
+                                            <li> <a href="#!" style="color: #a2c4c9; " class="link"> <?= $forecast['Nom11'] ?></a></li>
+                                        </ul>
+                                    </div>
                                 </div>
-                                </div>
                                 <div class="col-12">
-                                <div class="separation1 2">
-                                    <ul>
-                                    <li> <a href="#!" style="color: #a2c4c9; " class="link"> <?= $forecast['Nom12'] ?></a></li>
-                                    <li> <a href="#!" style="color: #a2c4c9; " class="link"> <?= $forecast['Nom13'] ?></a></li>
-                                    <li> <a href="#!" style="color: #a2c4c9; " class="link"> <?= $forecast['Nom14'] ?></a></li>
-                                    <li> <a href="#!" style="color: #a2c4c9; " class="link"> <?= $forecast['Nom15'] ?></a></li>
-                                    <li> <a href="#!" style="color: #a2c4c9; " class="link"> <?= $forecast['Nom16'] ?></a></li>
-                                    <li> <a href="#!" style="color: #a2c4c9; " class="link"> <?= $forecast['Nom17'] ?></a></li>
-                                    <li> <a href="#!" style="color: #a2c4c9; " class="link"> <?= $forecast['Nom18'] ?></a></li>
-                                    <li> <a href="#!" style="color: #a2c4c9; " class="link"> <?= $forecast['Nom19'] ?></a></li>
-                                    <li> <a href="#!" style="color: #a2c4c9; " class="link"> <?= $forecast['Nom20'] ?></a></li>
-                                    <li> <a href="#!" style="color: #a2c4c9; " class="link"> <?= $forecast['Nom21'] ?></a></li>
-                                    <li> <a href="#!" style="color: #a2c4c9; " class="link"> <?= $forecast['Nom22'] ?></a></li>
-                                    <li> <a href="#!" style="color: #a2c4c9; " class="link"> <?= $forecast['Nom23'] ?></a></li>
-                                    </ul>
+                                    <div class="separation1 2">
+                                        <ul>
+                                            <li> <a href="#!" style="color: #a2c4c9; " class="link"> <?= $forecast['Nom12'] ?></a></li>
+                                            <li> <a href="#!" style="color: #a2c4c9; " class="link"> <?= $forecast['Nom13'] ?></a></li>
+                                            <li> <a href="#!" style="color: #a2c4c9; " class="link"> <?= $forecast['Nom14'] ?></a></li>
+                                            <li> <a href="#!" style="color: #a2c4c9; " class="link"> <?= $forecast['Nom15'] ?></a></li>
+                                            <li> <a href="#!" style="color: #a2c4c9; " class="link"> <?= $forecast['Nom16'] ?></a></li>
+                                            <li> <a href="#!" style="color: #a2c4c9; " class="link"> <?= $forecast['Nom17'] ?></a></li>
+                                            <li> <a href="#!" style="color: #a2c4c9; " class="link"> <?= $forecast['Nom18'] ?></a></li>
+                                            <li> <a href="#!" style="color: #a2c4c9; " class="link"> <?= $forecast['Nom19'] ?></a></li>
+                                            <li> <a href="#!" style="color: #a2c4c9; " class="link"> <?= $forecast['Nom20'] ?></a></li>
+                                            <li> <a href="#!" style="color: #a2c4c9; " class="link"> <?= $forecast['Nom21'] ?></a></li>
+                                            <li> <a href="#!" style="color: #a2c4c9; " class="link"> <?= $forecast['Nom22'] ?></a></li>
+                                            <li> <a href="#!" style="color: #a2c4c9; " class="link"> <?= $forecast['Nom23'] ?></a></li>
+                                        </ul>
                                     </div>
                                 </div>
                             </div>
